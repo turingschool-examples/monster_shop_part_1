c06ffef871595b340db0d1386db71f0ccc5520da
msimon42 <msimon@uab.edu>
2019-12-16 16:19:31 -0700

    Add user class

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    name { "MyString" }
+    address { "MyString" }
+    city { "MyString" }
+    state { "MyString" }
+    zip { "MyString" }
+    email { "MyString" }
+    password { "MyString" }
+  end
+end
