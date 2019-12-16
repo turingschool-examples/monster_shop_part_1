8f638136f8d2c8334356dfc85baed53872d4bcd6
Kevin Krato <kratok08@gmail.com>
2019-12-16 13:48:16 -0700

    Add index, new, create actions to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def new
+	end
+
+	def create
+		User.create(user_params)
+		redirect_to	'/profile'
+	end
+
+	private
+		def user_params
+			params.permit(
+				:name, 
+				:street_address,
+				:city,
+				:state,
+				:zip,
+				:email,
+				:password_digest
+			)
+		end
 end
