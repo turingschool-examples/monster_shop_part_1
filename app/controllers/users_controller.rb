a46bf169fdbb16b8c1226a0479516e2c1ca62184
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-16 14:54:08 -0700

    Add session to keep user logged in
    Co-Authored-By: Linda Le <linda-le1@users.noreply.github.com>
    Co-Authored-By: Daniel "Danny" Moran  <danmoran-pro@users.noreply.github.com>
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>
    Co-Authored-By: Travis Borgsmiller <TravisBorgsmiller@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,17 @@ class UsersController < ApplicationController
 
   def create
     user = User.create(user_params)
-
     if user.save
       flash[:success] = "Welcome, #{user.name}! You're now registered!"
+      session[:user_id] = user.id
       redirect_to '/profile'
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(session[:user_id])
   end
 
   private
