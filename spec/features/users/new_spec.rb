3d99087d2a55e2f0935cf886d003e1d497ce70f5
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-16 14:01:30 -0700

    Add test for new user registration
    Co-Authored-By: Linda Le <linda-le1@users.noreply.github.com>
    Co-Authored-By: Daniel "Danny" Moran  <danmoran-pro@users.noreply.github.com>
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>
    Co-Authored-By: Travis Borgsmiller <TravisBorgsmiller@users.noreply.github.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -10,4 +10,32 @@ RSpec.describe 'As a visitor' do
 
     expect(current_path).to eq('/register')
   end
+
+  it "can register a new user" do
+    visit '/register'
+
+    name = 'Billy Bobby'
+    address = '123 Maine Street'
+    city = 'Denver'
+    state = 'CO'
+    zip = '80211'
+    email = 'billyboy@gmail.com'
+    password = 'secret'
+
+    fill_in :name, with: name
+    fill_in :address, with: address
+    fill_in :city, with: city
+    fill_in :state, with: state
+    fill_in :zip_code, with: zip
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password
+
+    click_button 'Create User'
+
+    flash = "Welcome, #{name}! You're now registered!"
+
+    expect(current_path).to eq('/profile')
+    expect(page).to have_content(flash)
+  end
 end
