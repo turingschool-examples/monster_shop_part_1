b17603027bbc7bcf4bfe2173417807c5fc0951bc
Alex Gallant <agallant1201@gmail.com>
2019-12-17 16:51:23 -0700

    Edit sessions create action
    Co-Authored-By: Kevin Krato <KevinKra@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,11 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-  user = User.find_by(name: params[:name])
+  user = User.find_by(email: params[:email])
   session[:user_id] = user.id
   flash[:success] = "Welcome, #{user.name}!"
-  redirect_to '/'
+		if user.admin?
+			redirect_to '/admin/profile'
+		end
 	end
 end
