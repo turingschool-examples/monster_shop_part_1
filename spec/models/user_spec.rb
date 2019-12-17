474414e29e6733684c82fd230cfaade079bb97bd
Madelyn Romero <m.romero013@gmail.com>
2019-12-17 14:54:45 -0700

    Add test to make user admin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of :email }
     it { should validate_presence_of :password }
   end
+
+  describe "roles" do
+    it "can be created as admin" do
+      user = User.create!(name: "Jordan",
+                          address: "394 High St",
+                          city: "Denver",
+                          state: "CO",
+                          zip_code: "80602",
+                          email: "hotones@hotmail.com",
+                          password: "password",
+                          password_confirmation: "password",
+                          role: 1)
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+  end
 end
