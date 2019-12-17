1fa77813b83fcd62a84285448f6ada880947ca96
Rachel Lew <rlew07@gmail.com>
2019-12-17 11:54:30 -0700

    Add expectation and validation for password_confirmation
    Co-Authored-By: Lain McGratH <lain.mcgrath@gusto.com>
    Co-Authored-By: Dylan Connolly <dylanconnolly145@gmail.com>
    Co-Authored-By: Santiago Caraveo <santiagocbrvg@hotmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :address, :city, :state, :zip
+  validates_presence_of :name, :address, :city, :state, :zip, :password_confirmation
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,6 @@ describe User, type: :model do
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
     it {should validate_presence_of :password}
+    it {should validate_presence_of :password_confirmation}
   end
 end
