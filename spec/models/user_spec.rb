718c608a8ab75de7260aa00478b82d0b74be7ecc
Ryan Allen <rallenwasd@gmail.com>
2019-12-17 14:52:17 -0700

    Refactor out uniqueness of name
    Co-Authored-By: sebastian <sebastiansloan94@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe User, type: :model do
 	describe 'validations' do
-
 		it {should validate_presence_of :name}
 		it {should validate_presence_of :street_address}
 		it {should validate_presence_of :city}
@@ -11,6 +10,5 @@ describe User, type: :model do
 		it {should validate_presence_of :email}
 		it {should validate_presence_of :password}
 		it {should validate_uniqueness_of :email}
-		it {should validate_uniqueness_of :name}
 	end
 end
