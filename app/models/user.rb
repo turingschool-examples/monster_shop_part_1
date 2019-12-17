a97a7fc877880bf271a40fd2fa9755474b5ff310
Ryan Allen <rallenwasd@gmail.com>
2019-12-17 14:54:06 -0700

    Refactor out uniqueness of name from model
    Co-Authored-By: sebastian <sebastiansloan94@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   validates_presence_of :name, :street_address, :city, :state, :zip, :email, :password
 
-	validates :name, uniqueness: true, presence: true
 	validates :email, uniqueness: true, presence: true
 	validates_presence_of :password, require: true
 
-	has_secure_password 
+	has_secure_password
 end
