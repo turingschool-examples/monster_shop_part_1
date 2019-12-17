77d7a9711d5b425e4a285130c445301a9c49181e
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-17 15:14:33 -0700

    Ad logic for redirect from login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
+    if user.default?
+      redirect_to '/profile'
+    elsif user.merchant?
+      redirect_to '/merchant/dashboard'
+    end
     session[:user_id] = user.id
     flash[:success] = "Welcome, #{user.name}, you are logged in!"
-    redirect_to '/profile'
   end
-
 end
