64f2ee08476bf12acc500f351edfb78f8b3a516b
msimon42 <msimon@uab.edu>
2019-12-16 20:04:21 -0700

    Add sad paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,33 @@
 class UsersController < ApplicationController
   def new
+     @form_info = session[:incomplete_user] || User.new(
+      name: 'name',
+      address: 'address',
+      city: 'city',
+      state: 'state',
+      zip: '00000',
+      email: 'email@email.com'
+    )
   end
 
   def create
-    new_user = User.new(user_params)
+    @new_user = User.new(user_params)
     if user_params[:password] != params[:confirm_password]
       flash.now[:warning]= "Please make sure your passwords match"
       render :new
     end
-    if new_user.save
+    if @new_user.save
       flash[:notice]= "Welcome #{new_user.name}"
-      session[:user_id] = new_user.id
+      session[:user_id] = @new_user.id
       redirect_to '/profile'
-    else
-      flash[:notice] = 'Please complete all required fields'
+    elsif !User.find_by_email(params[:email])
+      flash[:notice] = 'Please fill all required fields'
       redirect_back fallback_location: "/register"
+      session[:incomplete_user] = @new_user
+    else
+      flash[:notice] = 'Email address in use'
+      redirect_back fallback_location: "/register"
+      session[:incomplete_user] = @new_user
     end
   end
 
