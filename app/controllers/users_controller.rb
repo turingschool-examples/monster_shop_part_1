fcc7e5a357de1ed82cf19d4595a3680b2f1d3b4d
msimon42 <msimon@uab.edu>
2019-12-16 23:51:05 -0700

    Fix sad paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,17 @@
 class UsersController < ApplicationController
   def new
-     @form_info = session[:incomplete_user] || User.new(
-      name: 'name',
-      address: 'address',
-      city: 'city',
-      state: 'state',
-      zip: '00000',
-      email: 'email@email.com'
-    )
+    
   end
 
   def create
     @new_user = User.new(user_params)
-    if user_params[:password] != params[:confirm_password]
-      flash.now[:warning]= "Please make sure your passwords match"
-      render :new
-    end
     if @new_user.save
-      flash[:notice]= "Welcome #{new_user.name}"
+      flash[:notice]= "Welcome #{@new_user.name}"
       session[:user_id] = @new_user.id
       redirect_to '/profile'
-    elsif !User.find_by_email(params[:email])
-      flash[:notice] = 'Please fill all required fields'
-      redirect_back fallback_location: "/register"
-      session[:incomplete_user] = @new_user
     else
-      flash[:notice] = 'Email address in use'
-      redirect_back fallback_location: "/register"
-      session[:incomplete_user] = @new_user
+      flash[:notice] = @new_user.errors.full_messages.to_sentence
+      redirect_back fallback_location: '/register'
     end
   end
 
@@ -38,6 +22,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :address, :city, :state, :zip, :email, :password)
+    params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
   end
 end
