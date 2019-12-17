1fdb4d8b9fef9a8adfa3de87ddadad0b62670c59
Madelyn Romero <m.romero013@gmail.com>
2019-12-17 14:49:26 -0700

    Refactor with if/else statement for redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     session[:user_id] = user.id
     flash[:success] = "Welcome, #{user.name}, you are logged in!"
-    redirect_to '/profile'
+    if current_user.default?
+      redirect_to '/profile'
+    elsif current_admin?
+      redirect_to '/admin/dashboard'
+    end
   end
 
 end
