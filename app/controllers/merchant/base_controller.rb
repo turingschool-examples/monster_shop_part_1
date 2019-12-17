a7f93510cabb47568202ed35a1c9ef652275d40a
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-17 15:14:57 -0700

    Add base controller for before action

---
+++ app/controllers/merchant/base_controller.rb
@@ -0,0 +1,7 @@
+class Merchant::BaseController < ApplicationController
+  before_action :require_admin
+
+    def require_admin
+      render file: "/public/404" unless current_merchant?
+    end
+end
