3f23c4ffd1a8ef2b344316ac378ecf6ad0e3bae0
msimon42 <msimon@uab.edu>
2019-12-17 13:39:21 -0700

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/profile'
+      flash[:happy] = "Welcome, #{user.name}!"
+    else
+      flash[:sad] = 'Credentials were incorrect'
+      render :new
+    end   
+  end
+end
