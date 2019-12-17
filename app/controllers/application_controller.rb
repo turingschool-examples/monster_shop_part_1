2ce3c14e42188d38d468a863802a710f6151b373
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-17 15:15:05 -0700

    Add current merchant method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_merchant?
+    current_user && current_user.merchant?
+  end
 end
