deb2a8cce3cf1e04ac6384845082b89dc5c2100e
msimon42 <msimon@uab.edu>
2019-12-18 12:10:34 -0700

    Change redirect for 404

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,7 +2,7 @@ class Admin::BaseController < ApplicationController
   before_action :require_admin
 
   def require_admin
-    render file: '/public/404' unless current_user && current_user.admin?
+    render '/errors/404' unless current_user && current_user.admin?
   end
 
 
