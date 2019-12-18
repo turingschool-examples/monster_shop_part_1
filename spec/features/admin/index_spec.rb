476b5aa0680829ca9c3eabc56f0fd6153c925e43
Madelyn Romero <m.romero013@gmail.com>
2019-12-18 12:05:14 -0700

    Refactor with factory bot

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -2,16 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "As an admin" do
   it "it can login and be redirected to admin dashboard" do
-    user = User.create!(name: "Jordan",
-                        address: "394 High St",
-                        city: "Denver",
-                        state: "CO",
-                        zip_code: "80602",
-                        email: "hotones@hotmail.com",
-                        password: "password",
-                        password_confirmation: "password",
-                        role: 1)
-
+    user = create(:random_user, role: 1)
+  
     visit '/'
 
     click_link "Login"
@@ -19,7 +11,7 @@ RSpec.describe "As an admin" do
 
     fill_in :email, with: user.email
     fill_in :password, with: user.password
-    
+
     click_button "Login"
 
     expect(current_path).to eq("/admin/dashboard")
