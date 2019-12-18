825424bdaa235d1a77df35304a0ff746b0a0fee3
Madelyn Romero <m.romero013@gmail.com>
2019-12-18 15:21:20 -0700

    Add test for only active items displaying

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -54,5 +54,19 @@ RSpec.describe "Items Index Page" do
 
       expect(page).not_to have_css("#item-#{@dog_bone.id}")
     end
+
+    it "only displays active items" do
+      dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+
+      pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+      dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+
+      visit "/items"
+
+      expect(page).to have_css("#item-#{pull_toy.id}")
+      expect(page).to have_content(pull_toy.name)
+      expect(page).not_to have_css("#item-#{dog_bone.id}")
+      expect(page).not_to have_content(dog_bone.name)
+    end
   end
 end
