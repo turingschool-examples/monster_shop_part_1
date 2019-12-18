d47e68ed4ae4d3d663f19854ac618462ee838fbe
Alex Gallant <agallant1201@gmail.com>
2019-12-18 15:28:04 -0700

    Add if/else to accommodate different users
    Co-Authored-By: Kevin Krato <KevinKra@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,17 @@ class SessionsController < ApplicationController
   user = User.find_by(email: params[:email])
   session[:user_id] = user.id
   flash[:success] = "Welcome, #{user.name}!"
-		if user.admin?
-			redirect_to '/admin/profile'
-		elsif user.merchant?
-			redirect_to '/merchants/profile'
-		end
+	if user.admin?
+		redirect_to '/admin/profile'
+		flash[:success] = "Welcome, #{user.name}!"
+	elsif user.merchant?
+		redirect_to '/merchants/profile'
+		flash[:success] = "Welcome, #{user.name}!"
+	elsif user.default?
+		redirect_to '/users/profile'
+		flash[:success] = "Welcome, #{user.name}!"
+	else
+		redirect_to "/banana"
+	end
 	end
 end
