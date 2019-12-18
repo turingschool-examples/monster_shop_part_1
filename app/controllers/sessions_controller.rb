77265e24b675265118abea0d91797abddf4c1bec
Alex Gallant <agallant1201@gmail.com>
2019-12-18 16:09:13 -0700

    Change flash messages
    Co-Authored-By: Kevin Krato <KevinKra@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,13 @@ class SessionsController < ApplicationController
   flash[:success] = "Welcome, #{user.name}!"
 	if user.admin?
 		redirect_to '/admin/profile'
-		flash[:success] = "Welcome, #{user.name}!"
+		flash[:success] = "Welcome admin, #{user.name}!"
 	elsif user.merchant?
 		redirect_to '/merchants/profile'
-		flash[:success] = "Welcome, #{user.name}!"
+		flash[:success] = "Welcome merchant, #{user.name}!"
 	elsif user.default?
 		redirect_to '/users/profile'
-		flash[:success] = "Welcome, #{user.name}!"
+		flash[:success] = "Welcome user, #{user.name}!"
 	else
 		redirect_to "/"
 	end
