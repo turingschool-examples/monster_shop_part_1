4249a6b1ba97307bb5cfa43c60f9770a582ff3f0
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-12-17 17:31:12 -0700

    add routes and functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     login(user)
-    
+
     session[:user_id] = user.id
     flash[:success] = "Welcome, #{user.name}, you are logged in!"
+  end
+
+  def destroy
+    User.find(session[:user_id]).destroy
+    session[:user_id] = nil
+    session.delete(:cart)
+    flash[:success] = "You have been logged out"
+    redirect_to '/'
   end
 
   private
