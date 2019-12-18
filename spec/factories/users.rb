01faa38cfbaf12af6d1d2381800cfa67c0a3f363
msimon42 <msimon@uab.edu>
2019-12-18 15:36:16 -0700

    Add factory for test reg user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -30,4 +30,15 @@ FactoryBot.define do
     password {'password'}
     role {1}
   end
+
+  factory :random_reg_user_test, class: User do
+    name {Faker::Name.name_with_middle}
+    address {Faker::Address.street_address}
+    city {Faker::Address.city}
+    state {Faker::Address.state}
+    zip {Faker::Address.zip_code}
+    email {Faker::Internet.email}
+    password {'password'}
+    role {0}
+  end
 end
