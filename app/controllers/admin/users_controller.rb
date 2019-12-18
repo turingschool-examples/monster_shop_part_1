74075e8cf6eefbc291e5c72bcdbcad9bf4b68cfe
Alex Gallant <agallant1201@gmail.com>
2019-12-17 17:07:20 -0700

    Edit admin users controller
    Co-Authored-By: Kevin Krato <KevinKra@users.noreply.github.com>

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,6 @@
-class Admin::UsersController < ApplicationController
-  before_action :require_admin
-  
+class Admin::UsersController < Admin::BaseController
+
   def show
   end
 
-  private
-    def require_admin
-      render file: "/public/404" unless current_admin?
-    end
 end
