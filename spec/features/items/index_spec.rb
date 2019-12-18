3b941919978835f7998b5eea9d188b7246c773a0
Madelyn Romero <m.romero013@gmail.com>
2019-12-18 16:07:43 -0700

    Add test for turning item image into link to item show page

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -68,5 +68,17 @@ RSpec.describe "Items Index Page" do
       expect(page).not_to have_css("#item-#{dog_bone.id}")
       expect(page).not_to have_content(dog_bone.name)
     end
+
+    it "can click item image to go to item show page" do
+      dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+
+      pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+
+      visit "/items"
+
+      click_link "#{pull_toy.id}-image"
+
+      expect(current_path).to eq("/items/#{pull_toy.id}")
+    end
   end
 end
