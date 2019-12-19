cca3c6dbb0e783e95828e077a7c485de0471d1cd
Ryan Allen <rallenwasd@gmail.com>
2019-12-19 15:12:25 -0700

    Refactor to one lines
    Co-Authored-By: Alex Gallant <agallant1201@gmail.com>
    Co-Authored-By: Kevin Krato <kratok08@gmail.com>
    Co-Authored-By: sasloan <sebastiansloan94@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart
+  helper_method :cart, :current_user, :current_default?, :current_admin?, :current_user_name
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
   end
-
-	helper_method :current_user, :current_default?, :current_admin?, :current_user_name
 
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
 
   def current_admin?
-    return false unless session[:user_id]
-    user = User.find(session[:user_id])
-    user.admin?
+    current_user && current_user.admin?
   end
 
   def current_merchant?
-    return false unless session[:user_id]
-    user = User.find(session[:user_id])
-    user.merchant?
+    current_user && current_user.merchant?
   end
 
   def current_default?
-    return false unless session[:user_id]
-    user = User.find(session[:user_id])
-    user.default?
-  end
-
-  def current_user_name
-    user = User.find(session[:user_id])
-    user.name
+    current_user && current_user.default?
   end
 
 end
