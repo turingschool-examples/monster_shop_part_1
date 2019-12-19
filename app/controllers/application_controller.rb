6f9f673397ad6701754c9eb6f22c5ad6ae6b52a5
Ryan Allen <rallenwasd@gmail.com>
2019-12-19 13:09:16 -0700

    Refactor application controller with all methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
   end
 
-	helper_method :current_user, :current_default?, :current_admin?
+	helper_method :current_user, :current_default?, :current_admin?, :current_user_name
 
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -15,19 +15,25 @@ class ApplicationController < ActionController::Base
 
   def current_admin?
     return false unless session[:user_id]
-    @user = User.find(session[:user_id])
-    @user.admin?
+    user = User.find(session[:user_id])
+    user.admin?
   end
 
   def current_merchant?
     return false unless session[:user_id]
-    merch = User.find(session[:user_id])
-    merch.merchant?
+    user = User.find(session[:user_id])
+    user.merchant?
   end
 
   def current_default?
     return false unless session[:user_id]
-    @user = User.find(session[:user_id])
-    @user.default?
+    user = User.find(session[:user_id])
+    user.default?
   end
+
+  def current_user_name
+    user = User.find(session[:user_id])
+    user.name
+  end
+
 end
