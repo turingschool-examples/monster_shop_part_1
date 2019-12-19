d3476630eed669a3cf33e6d6338c2d186c4ec5d1
Kevin Krato <kratok08@gmail.com>
2019-12-19 07:39:33 -0700

    Delete user redundant factories, consolidate in user factory with conditionals

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :merchant do
+  factory :user do
     name  { Faker::Name.first_name }
     street_address { Faker::Address.street_address }
     city { Faker::Address.city }
@@ -7,6 +7,14 @@ FactoryBot.define do
     zip { Faker::Address.zip }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
-    role { 2 }
+    trait :default_user do
+      role { 0 }
+    end
+    trait :admin_user do
+      role { 1 }
+    end
+    trait :merchant_user do
+      role { 2 }
+    end
   end
 end
