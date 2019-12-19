b04bff463b94d535298a205ee1a2f9ac093f8dac
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-12-19 15:46:47 -0700

    add test for user story 23 and 24
    Co-Authored-By: Linda Le <54052410+linda-le1@users.noreply.github.com>

--- spec/features/cart/add_spec.rb
+++ spec/features/cart/add_spec.rb
@@ -33,4 +33,47 @@ RSpec.describe 'Cart creation' do
       end
     end
   end
+  describe 'when I visit the cart show page' do
+    before :each do
+      @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+    end
+    it 'increments items that are in the cart, and can not increment beyond item inventory amount' do
+
+      visit "/items/#{@pencil.id}"
+      click_on "Add To Cart"
+
+      visit "/items/#{@paper.id}"
+      click_on "Add To Cart"
+
+      visit '/cart'
+
+      within "#cart-item-#{@paper.id}" do
+        click_button "+"
+        expect(page).to have_content("2")
+        click_button "+"
+        expect(page).to have_content("3")
+        click_button "+"
+        expect(page).to have_content("3")
+      end
+    end
+    it 'decrements items that are in the cart, and if reaches 0 then item is removed from cart' do
+
+      visit "/items/#{@paper.id}"
+      click_on "Add To Cart"
+
+      visit '/cart'
+
+      within "#cart-item-#{@paper.id}" do
+        click_button "+"
+        expect(page).to have_content("2")
+        click_button "-"
+        expect(page).to have_content("1")
+        click_button "-"
+      end
+
+      expect(page).to_not have_content(@pencil.name)
+    end
+  end
 end
