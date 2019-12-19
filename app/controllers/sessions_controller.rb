88f80fb2813de3d6e093ece130c5ad26bba4d355
Ryan Allen <rallenwasd@gmail.com>
2019-12-19 13:02:50 -0700

    Refactor redirects to match user stories

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,10 @@ class SessionsController < ApplicationController
   session[:user_id] = user.id
   flash[:success] = "Welcome, #{user.name}!"
 	if user.admin?
-		redirect_to '/admin/profile'
+		redirect_to '/admin/dashboard'
 		flash[:success] = "Welcome admin, #{user.name}!"
 	elsif user.merchant?
-		redirect_to '/merchants/profile'
+		redirect_to '/merchants/dashboard'
 		flash[:success] = "Welcome merchant, #{user.name}!"
 	elsif user.default?
 		redirect_to '/users/profile'
