15d2256c81825bef82d39b37a436238c65816870
Ryan Allen <rallenwasd@gmail.com>
2019-12-19 06:47:12 -0700

    Refactor current_default to match user story

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,26 @@ class ApplicationController < ActionController::Base
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
   end
 
-	helper_method :current_user
+	helper_method :current_user, :current_default?
 
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
 
   def current_admin?
+    return false unless session[:user_id]
     current_user && current_user.admin?
   end
 
   def current_merchant?
-    current_user && current_user.merchant?
+    return false unless session[:user_id]
+    merch = User.find(session[:user_id])
+    merch.merchant?
   end
 
   def current_default?
-    current_user && current_user.default?
+    return false unless session[:user_id]
+    @user = User.find(session[:user_id])
+    @user.default?
   end
 end
