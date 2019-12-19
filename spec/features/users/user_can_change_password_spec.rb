96bc68941dc0142f419fec1e55e01f0688965cba
Madelyn Romero <m.romero013@gmail.com>
2019-12-19 15:32:36 -0700

    Create test for changing password

--- spec/features/users/user_can_change_password_spec.rb
+++ spec/features/users/user_can_change_password_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "a user can visit their profile page" do
   it "can click a link to change their password" do
-    user = create(:random_user)
+    user = create(:random_user, password: "password", password_confirmation: "password")
 
     visit "/login"
     fill_in :email, with: user.email
@@ -12,7 +12,24 @@ RSpec.describe "a user can visit their profile page" do
     visit "/profile"
 
     click_link("Change Password")
-    expect(current_path).to eq()
+    expect(current_path).to eq("/user/password/edit")
 
+    password = new_password
+
+    fill_in :new_password, with: password
+    fill_in :new_password_confirmation, with: password
+
+    click_button "Create New Password"
+
+    expect(current_path).to eq("/profile")
+    expect(page).to have_content("Your password has been updated.")
+
+    click_link "Log Out"
+    click_link "Login"
+    fill_in :email, with: user.email
+    fill_in :password, with: password
+    click_on "Login"
+    expect(current_path).to eq("/profile")
+    expect(page).to have_content("Welcome, #{user.name}, you are logged in!")
   end
 end
