976a5830a04116e90e05d98b8e420cc5054960c6
Kevin Krato <kratok08@gmail.com>
2019-12-19 06:56:34 -0700

    Create destroy method to logout users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,23 @@ class SessionsController < ApplicationController
   user = User.find_by(email: params[:email])
   session[:user_id] = user.id
   flash[:success] = "Welcome, #{user.name}!"
-	if user.admin?
-		redirect_to '/admin/profile'
-		flash[:success] = "Welcome admin, #{user.name}!"
-	elsif user.merchant?
-		redirect_to '/merchants/profile'
-		flash[:success] = "Welcome merchant, #{user.name}!"
-	elsif user.default?
-		redirect_to '/users/profile'
-		flash[:success] = "Welcome user, #{user.name}!"
-	else
-		redirect_to "/"
-	end
+    if user.admin?
+      redirect_to '/admin/profile'
+      flash[:success] = "Welcome admin, #{user.name}!"
+    elsif user.merchant?
+      redirect_to '/merchants/profile'
+      flash[:success] = "Welcome merchant, #{user.name}!"
+    elsif user.default?
+      redirect_to '/users/profile'
+      flash[:success] = "Welcome user, #{user.name}!"
+    else
+      redirect_to "/"
+    end
+  end
+  
+  def destroy
+    session.delete(:user_id)
+    flash[:success] = "You have been signed out."
+    redirect_to "/welcome/home"
 	end
 end
