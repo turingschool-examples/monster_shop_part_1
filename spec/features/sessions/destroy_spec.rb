b176d3d726bcf3d15a18fcd50e5d28c3e8561937
Kevin Krato <kratok08@gmail.com>
2019-12-19 07:40:28 -0700

    Happy path feature test routes and messages for user, merchant, admin sign in.

--- spec/features/sessions/destroy_spec.rb
+++ spec/features/sessions/destroy_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'User logging out', type: :feature do
-  let!(:user) { create(:user) }
+  let!(:user) { create(:user, :default_user) }
   before {
     visit "/login"
 
