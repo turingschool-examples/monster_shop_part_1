c64f5d564011d8cc6a5df5e7dd1b2e3ece200d3b
Madelyn Romero <m.romero013@gmail.com>
2019-12-19 16:38:35 -0700

    Add if else for sad path password and password confirmation being the same

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,8 +27,14 @@ class UsersController < ApplicationController
   end
 
   def update
-    redirect_to "/profile"
-    flash[:notice] = "Your password has been updated."
+    if params.include?(:password) && params[:password] == params[:password_confirmation]
+      current_user.update(user_params)
+      redirect_to "/profile"
+      flash[:notice] = "Your password has been updated."
+    elsif params[:password] != params[:password_confirmation]
+      redirect_to "/user/password/edit"
+      flash[:error] = "Password and password confirmation do not match."
+    end
   end
 
   private
