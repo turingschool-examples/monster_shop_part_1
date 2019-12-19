f2672b048d38fc97ed3eae788b56adbf9e4a3c7f
Alex Gallant <agallant1201@gmail.com>
2019-12-18 17:54:18 -0700

    Add condition to merchant controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,25 @@ class ApplicationController < ActionController::Base
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
   end
 
-	helper_method :current_user
+	helper_method :current_user, :current_merchant?
 
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
 
   def current_admin?
+    return false unless session[:user_id]
     current_user && current_user.admin?
   end
 
   def current_merchant?
-    current_user && current_user.merchant?
+    return false unless session[:user_id]
+    merch = User.find(session[:user_id])
+    merch.merchant?
   end
 
   def current_default?
+    return false unless session[:user_id]
     current_user && current_user.default?
   end
 end
