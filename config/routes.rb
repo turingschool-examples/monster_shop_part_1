bfa8507681d9b194cf7786af2db8098f3932091c
Daniel <danmoran.pro@gmail.com>
2019-12-20 11:04:55 -0700

    Add link to my orders for users.
    Co-authored-by: Linda Le <54052410+linda-le1@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,7 @@ Rails.application.routes.draw do
   get '/profile', to: 'users#show'
   get 'profile/edit', to:'users#edit'
   patch 'profile/edit', to:'users#update'
+  get  '/profile/orders', to:'userorders#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
--- spec/features/users/user_spec.rb
+++ spec/features/users/user_spec.rb
@@ -40,4 +40,49 @@ RSpec.describe 'As a user' do
 
     expect(page).to have_link("Edit Profile")
   end
+
+  it "can display users order link from user's profile" do
+
+    user = create(:random_user, role: 0)
+    meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+    tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    pull_toy = brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    dog_bone = brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+    new_order = Order.create(name:"Bert",address: "123 Sesame St.", city: "NYC", state: "New York", zip: 10001)
+    visit '/'
+
+    click_link "Login"
+    expect(current_path).to eq('/login')
+
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+
+    click_button "Login"
+
+    visit "/items/#{tire.id}"
+    click_on "Add To Cart"
+    visit "/items/#{pull_toy.id}"
+    click_on "Add To Cart"
+
+    visit "/cart"
+    click_on "Checkout"
+    click_button "Create Order"
+
+    visit '/profile'
+
+    expect(page).to have_link("My Orders")
+
+    click_on "My Orders"
+
+    expect(current_path).to eq("/profile/orders")
+  end
 end
+  #
+  # User Story 27, User Profile displays Orders link
+  #
+  # As a registered user
+  # When I visit my Profile page
+  # And I have orders placed in the system
+  # Then I see a link on my profile page called "My Orders"
+  # When I click this link my URI path is "/profile/orders"
