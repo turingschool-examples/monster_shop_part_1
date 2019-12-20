62f14c22b9c808460ea54fdeb87cff26265790a8
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-19 17:36:13 -0700

    Add belongs_to and has_many to models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,14 @@
 class Order <ApplicationRecord
-  validates_presence_of :name, :address, :city, :state, :zip
+  validates_presence_of :name,
+                        :address,
+                        :city,
+                        :state,
+                        :zip
 
   has_many :item_orders
   has_many :items, through: :item_orders
+
+  belongs_to :user
 
   enum status: ["pending", "packaged", "shipped", "cancelled"]
 
