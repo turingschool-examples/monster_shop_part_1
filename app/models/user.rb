26a3ab5e7ed4550c741cb95e7e92a4792b04ad0f
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-21 08:53:19 -0700

    Set up one-to-many relationship between merchant and users. Write model tests for merchant and user models for relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
   validates_presence_of :password_confirmation, require: true, :on => :create
 
   has_many :orders
-  
+
+  belongs_to :merchant, optional: true
+
   has_secure_password
 
   enum role: ["user", "merchant_employee", "merchant_admin", "admin"]
