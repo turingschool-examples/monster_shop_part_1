f763107ae76458d9cb098d4ab6250c597152c698
Madelyn Romero <m.romero013@gmail.com>
2019-12-22 16:31:23 -0700

    Swap enum values so defaults to unfulfilled
    Co-Authored-By: Linda Le <54052410+linda-le1@users.noreply.github.com>

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -4,7 +4,7 @@ class ItemOrder <ApplicationRecord
   belongs_to :item
   belongs_to :order
 
-  enum status: %w(fulfilled unfulfilled)
+  enum status: %w(unfulfilled fulfilled)
 
   def subtotal
     price * quantity
