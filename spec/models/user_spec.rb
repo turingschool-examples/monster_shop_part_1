3fc46e24fd2b9073d679a51befed3ca1333b7fef
Madelyn Romero <m.romero013@gmail.com>
2019-12-22 20:32:03 -0700

    Add one to many relationship to tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe User, type: :model do
 
   describe 'relationships' do
     it { should have_many :orders}
+    it {should belong_to(:merchant).optional}
   end
 
   describe 'roles' do
