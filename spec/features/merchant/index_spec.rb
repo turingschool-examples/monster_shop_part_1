b0f0bcc3509da5104c6c02b7bc39a38620a3d8b5
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-12-23 12:49:59 -0700

    Add merchant items link test

--- spec/features/merchant/index_spec.rb
+++ spec/features/merchant/index_spec.rb
@@ -92,4 +92,39 @@ RSpec.describe 'as a merchant', type: :feature do
     expect(page).to have_content(merchant_company.state)
     expect(page).to have_content(merchant_company.zip)
   end
+
+  it 'has link to view own merchant items that routes to merchant items page' do
+
+    merchant_company = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+
+    merchant_admin = User.create(name: "Jordan",
+                           address: "394 High St",
+                           city: "Denver",
+                           state: "CO",
+                           zip_code: "80602",
+                           email: "hotthrees@hotmail.com",
+                           password: "password",
+                           password_confirmation: "password",
+                           role: 2)
+
+    merchant_employee = User.create(name: "Jordi",
+                           address: "394 High St",
+                           city: "Denver",
+                           state: "CO",
+                           zip_code: "80602",
+                           email: "hotfours@hotmail.com",
+                           password: "password",
+                           password_confirmation: "password",
+                           role: 3)
+
+    merchant_company.users << [merchant_employee, merchant_admin]
+
+    visit '/login'
+    fill_in :email, with: merchant_admin.email
+    fill_in :password, with: merchant_admin.password
+    click_button "Login"
+
+    expect(page).to have_link "See My Items"
+  end
+
 end
