ba19d2ae1dd54f1e48420d183b7d6eb08ab06fa4
Madelyn Romero <m.romero013@gmail.com>
2019-12-23 14:08:55 -0700

    Create route for dashboard update orders

--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/dashboard', to: 'dashboard#index'
     get '/profile', to: 'users#index'
+    patch '/orders/:id', to: 'dashboard#update'
   end
 
   get '/user/password/edit', to: 'users#password_edit'
