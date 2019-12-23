b544576ed618478808f88d1dfa137d4e6484aa6f
Madelyn Romero <m.romero013@gmail.com>
2019-12-22 19:47:43 -0700

    Update test for merchant employee login going to merchant dashboard

--- spec/features/merchant/index_spec.rb
+++ spec/features/merchant/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'as a merchant', type: :feature do
   it "redirects me to merchant dashboard after login" do
-    merchant = User.create(name: "Jordan",
+    merchant_admin = User.create(name: "Jordan",
                            address: "394 High St",
                            city: "Denver",
                            state: "CO",
@@ -12,15 +12,32 @@ RSpec.describe 'as a merchant', type: :feature do
                            password_confirmation: "password",
                            role: 2)
 
+    merchant_employee = User.create(name: "Jordi",
+                           address: "394 High St",
+                           city: "Denver",
+                           state: "CO",
+                           zip_code: "80602",
+                           email: "hotfours@hotmail.com",
+                           password: "password",
+                           password_confirmation: "password",
+                           role: 3)
+
     visit '/login'
-
-    fill_in :email, with: merchant.email
-    fill_in :password, with: merchant.password
-
+    fill_in :email, with: merchant_admin.email
+    fill_in :password, with: merchant_admin.password
     click_button "Login"
 
     expect(current_path).to eq('/merchant/dashboard')
-    expect(page).to have_content("Welcome, #{merchant.name}, you are logged in!")
+    expect(page).to have_content("Welcome, #{merchant_admin.name}, you are logged in!")
+    click_link "Log Out"
+
+    visit '/login'
+    fill_in :email, with: merchant_employee.email
+    fill_in :password, with: merchant_employee.password
+    click_button "Login"
+
+    expect(current_path).to eq('/merchant/dashboard')
+    expect(page).to have_content("Welcome, #{merchant_employee.name}, you are logged in!")
   end
 
   it "displays full address of the merchant I work for" do
@@ -50,10 +67,5 @@ RSpec.describe 'as a merchant', type: :feature do
     fill_in :password, with: merchant_admin.password
 
     click_button "Login"
-
-    visit "/merchant/dashboard"
-
-    expect(page).to have_content(merchant_admin.name)
-  #   expect(page).to have_content(merchant.address)
   end
 end
