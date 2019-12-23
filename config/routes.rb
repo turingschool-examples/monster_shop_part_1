65ef5dc8fb6b4021a18857c5c8bac8bb47ba17d0
msimon42 <msimon@uab.edu>
2019-12-22 23:31:46 -0600

    Remove useless route

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   get '/profile/orders', to: 'orders#index'
   get '/profile/orders/:id', to: 'orders#show'
   patch '/profile/orders/:id', to: 'order_status#update'
-  patch '/admin/orders/:id', to: 'order_status#update'
+
 
   get '/register', to: 'users#new'
   post '/register', to: "users#create"
