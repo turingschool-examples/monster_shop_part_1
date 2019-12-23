9049cc76aa31d561e4b93f5f0313524519f563f3
Darren Campbell <darren2802@gmail.com>
2019-12-23 08:42:58 -0700

    Add functionality for user story 31 all merchants fulfill items on order

--- app/models/order.rb
+++ app/models/order.rb
@@ -26,4 +26,12 @@ class Order < ApplicationRecord
   def self.packaged
     self.where(status: 'Packaged')
   end
+
+  def order_status
+    if self.item_orders.all? { |item_order| item_order.fulfilled_by_merchant == true }
+      'Packaged'
+    else
+      self.status
+    end
+  end
 end
