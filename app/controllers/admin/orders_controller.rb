b0967c02e7c2a1af70ef281d259c545a2dba7ab6
msimon42 <msimon@uab.edu>
2019-12-23 22:53:19 -0600

    ditto

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,6 +11,9 @@ class Admin::OrdersController < Admin::BaseController
     order = Order.find(params[:id])
     if (params[:status] == 'cancelled') && order.status != 'Shipped'
       order.update(status: 3)
+      order.item_orders.each do |item_order|
+        item_order.item.update(inventory: (item_order.item.inventory + item_order.quantity))
+      end   
       flash[:happy] = 'Your order has been cancelled'
       redirect_back fallback_location: "/profile/orders/#{order.id}"
     elsif params[:status] == 'shipped'
