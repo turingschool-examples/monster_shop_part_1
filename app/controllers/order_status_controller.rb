c42216363d652d04d42afc6d34113f7bed6d41a4
msimon42 <msimon@uab.edu>
2019-12-23 22:53:10 -0600

    Adds back inventory when order is cancelled

--- app/controllers/order_status_controller.rb
+++ app/controllers/order_status_controller.rb
@@ -3,6 +3,9 @@ class OrderStatusController < ApplicationController
     order = Order.find(params[:id])
     if (params[:status] == 'cancelled') && order.status != 'Shipped'
       order.update(status: 3)
+      order.item_orders.each do |item_order|
+        item_order.item.update(inventory: (item_order.item.inventory + item_order.quantity))
+      end   
       flash[:happy] = 'Your order has been cancelled'
       redirect_back fallback_location: "/profile/orders/#{order.id}"
     elsif params[:status] == 'shipped'
