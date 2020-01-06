ff3e066f67de0a40a62aa8211a9e0aea55461dc4
msimon42 <msimon@uab.edu>
2020-01-06 14:32:44 -0700

    Add method to activate and deactivate merchant

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,7 +1,27 @@
 class Admin::MerchantsController < Admin::BaseController
 
+  def index
+    @path = '/admin/'
+    @merchants = Merchant.all
+  end
+
   def show
     @merchant = Merchant.find(params[:merchant_id])
-  end 
-   
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    if params[:status] == 'deactivate'
+      merchant.update(active?: false)
+      merchant.items.update(active?: false)
+      flash[:happy] = 'Merchant Deactivated'
+      redirect_back fallback_location: '/admin/merchants'
+    else
+      merchant.update(active?: true)
+      merchant.items.update(active?: true)
+      flash[:happy] = 'Merchant Activated'
+      redirect_back fallback_location: '/admin/merchants'
+    end
+  end
+
 end
