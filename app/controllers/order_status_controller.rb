dba0530e0450c8864067cb6e6e1b7bf02d9e43df
msimon42 <msimon@uab.edu>
2020-01-07 09:58:59 -0700

    Update redirect for order cancellation

--- app/controllers/order_status_controller.rb
+++ app/controllers/order_status_controller.rb
@@ -5,9 +5,9 @@ class OrderStatusController < ApplicationController
       order.update(status: 3)
       order.item_orders.each do |item_order|
         item_order.item.update(inventory: (item_order.item.inventory + item_order.quantity))
-      end   
+      end
       flash[:happy] = 'Your order has been cancelled'
-      redirect_back fallback_location: "/profile/orders/#{order.id}"
+      redirect_to '/profile'
     elsif params[:status] == 'shipped'
       order.update(status: 2)
       flash[:happy] = 'Order has been shipped'
