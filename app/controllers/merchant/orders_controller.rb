385fa982cf5095e5a583935a65260b43c484295b
Paul Debevec <paulmdebevec@gmail.com>
2020-01-07 12:24:03 -0700

    Broke out helper methods to shorten method length
    Co-Authored-By: msimon42 <52684225+msimon42@users.noreply.github.com>
    Co-Authored-By: darren2802 <37692413+darren2802@users.noreply.github.com>
    Co-Authored-By: Joshua O'bannon <jtobannon@gmail.com>

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -11,15 +11,30 @@ class Merchant::OrdersController < Merchant::BaseController
     item_order = ItemOrder.find(params[:item_order_id])
     item = Item.find(item_order.item_id)
     if item_order.fulfilled_by_merchant
-      item_order.update(fulfilled_by_merchant: false)
-      item.update(inventory: (item.inventory + item_order.quantity))
-      flash[:sad] = 'Item unfulfilled'
+      unfulfill(item_order, item)
     else
+      fulfill(item_order, item)
+    end
+    redirect_to "/merchant/orders/#{item_order.order_id}"
+  end
+
+  private
+    def check_fulfilled?(item_order)
+      order = Order.find(item_order.order_id)
+      package_order = order.item_orders.all? {|item_ord| item_ord.fulfilled_by_merchant}
+      order.update(status: "Packaged") if package_order
+    end
+
+    def fulfill(item_order, item)
       item_order.update(fulfilled_by_merchant: true)
       item.update(inventory: (item.inventory - item_order.quantity))
       flash[:happy] = 'Item fulfilled'
+      check_fulfilled?(item_order)
     end
 
-    redirect_to "/merchant/orders/#{item_order.order_id}"
-  end
+    def unfulfill(item_order, item)
+      item_order.update(fulfilled_by_merchant: false)
+      item.update(inventory: (item.inventory + item_order.quantity))
+      flash[:sad] = 'Item unfulfilled'
+    end
 end
